060f13d4e0f401688c2b37447dec529362368974
Gean Victor <geanvictorduque@gmail.com>
2023-12-02 16:38:25 -0300

    refactor: Return person object

--- src/repositories/person.js
+++ src/repositories/person.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose')
 const Person = mongoose.model('Person');
 
-exports.find = async (query, select = null) => {
+exports.find = async (query, select = {photo: 0, photoUrl: 0}) => {
     try {
         let person = await Person.find(query).select(select);
         return person[0];
