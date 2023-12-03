5cc050e5c63bc5a21d36f2b740540392e0c1b19c
Gean Victor <geanvictorduque@gmail.com>
2023-12-03 13:07:58 -0300

    refactor: Refactor findCondominiumByPerson

--- src/repositories/condominium.js
+++ src/repositories/condominium.js
@@ -3,9 +3,9 @@
 const mongoose = require('mongoose');
 const Condominium = mongoose.model('Condominium');
 
-exports.find = async (query, select = null) => {
+exports.find = async (query, select = {_id: 1, name: 1}) => {
     try {
-        let condominiums = await Condominium.find(query).select({ _id: 1, name: 1 });
+        let condominiums = await Condominium.find(query).select(select);
         return condominiums;
     } catch (error) {
         return error.message;
