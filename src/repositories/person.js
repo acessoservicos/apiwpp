ddf17cd8dad2f47c05b81b58b2b1a8e89693831d
Gean Victor <geanvictorduque@gmail.com>
2023-12-03 21:03:09 -0300

    refactor: Refactor findPersonByPhone

--- src/repositories/person.js
+++ src/repositories/person.js
@@ -3,9 +3,10 @@
 const mongoose = require('mongoose')
 const Person = mongoose.model('Person');
 
-exports.find = async (query, select = {photo: 0, photoUrl: 0}) => {
+exports.find = async (query, select = {_id: 1, name: 1}) => {
     try {
         let person = await Person.find(query).select(select);
+        person[0].name = person[0].name.trim().split(" ")[0];
         return person[0];
     } catch (error) {
         return error.message;
